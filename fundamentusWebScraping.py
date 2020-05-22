b1118d22dd06283a448e850c79f68c08a423df43
yuri10 <oliveirayuri10@hotmail.com>
2020-05-22 20:50:28 -0300

    falta ajeitar o tratamento das tabelas

--- fundamentusWebScraping.py
+++ fundamentusWebScraping.py
@@ -17,24 +17,46 @@ from selenium.common.exceptions import TimeoutException
 import time
 #from selenium.webdriver.common.action_chains import ActionChains
 
-with open('C:/Users/Yuri Oliveira/Desktop/BolsaDeValores/codigos_b3.txt') as f:
-    codigos = f.readlines()
+with open('C:/Users/Yuri Oliveira/Desktop/BolsaDeValoresB3/codigos_b3.txt') as f:
+    codes = f.read().splitlines()
     f.close()
 
+#removing \n from codes
+#for codes in
 
 #options of web driver
 options = webdriver.ChromeOptions()
 options.add_argument("--start-maximized")
 #actions = ActionChains(driver)
-driver = webdriver.Chrome("C:/Users/Yuri Oliveira/Desktop/assistfly/chromedriver.exe", chrome_options = options)
+driver = webdriver.Chrome("C:/chromedriver.exe", chrome_options = options)
 
+code = codes
+for code in codes:
 
-driver.get("https://www.fundamentus.com.br/detalhes.php?papel=B3SA3")
-
-driver.execute_script("window.open('https://www.fundamentus.com.br/detalhes.php?papel=' + codigo);")
-
-page_source = driver.page_source                                                              
-#utiliza o pandas para encontrar as tabelas do html
-df = pd.read_html(page_source)                                                       
+    url = "'https://www.fundamentus.com.br/detalhes.php?papel=" + code + "'"
+    driver.execute_script("window.open("+ url + ");")
+    driver.close()
+    driver.switch_to.window(driver.window_handles[0])
+    
+    window_name = driver.window_handles[0]
+    
+    try:
+        #pega o html da pagina atual
+        page_source = driver.page_source                                                              
+        #utiliza o pandas para encontrar as tabelas do html
+        df = pd.read_html(page_source)
+        
+        #Pega só a tabela dos indicadores fundamenalista
+        dfIndicFund = df[2]
+        dfIndicFund = dfIndicFund.loc[:, 2:]
+        
+        dfIndicFund2 = dfIndicFund.loc[:, 2:3]
+        dfIndicFund = dfIndicFund.loc[:, 4:5]
+        dfIndicFund = dfIndicFund.T
+        
+        
+    except:
+        print("A pagina com codigo " + code +" nao possui tabelas!")
+                                                       
 
 
